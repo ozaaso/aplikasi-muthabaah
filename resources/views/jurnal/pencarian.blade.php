3f7182da41e7fea64f10d3e65c94d985fc3a5b78
fijarixar <fijar.alridho@customs.go.id>
2024-10-05 19:40:06 +0700

    delete paging pencarian

--- resources/views/jurnal/pencarian.blade.php
+++ resources/views/jurnal/pencarian.blade.php
@@ -37,15 +37,7 @@
         @endforeach
 
 
-                <!-- Pagination -->
-                <div class="pagination">
-                <a href="#">&laquo;</a> <!-- Previous -->
-                <a href="#">1</a>
-                <a href="#">2</a>
-                <a href="#">3</a>
-                <!-- <a href="#">4</a>
-                <a href="#">5</a> -->
-                <a href="#">&raquo;</a> <!-- Next -->
+
             </div>
 
     </div>
