d599e29a4fba376bcea1dacd17c5b973784b9f37
ozaaso <ozaaso@gmail.com>
2025-02-14 14:35:39 +0700

    tab edit

--- resources/views/layout/tab.blade.php
+++ resources/views/layout/tab.blade.php
@@ -2,13 +2,13 @@
 <div class="tab-bar-container">
     <div class="tab-bar">
         <div class="tab" id="tab-harian">
-            <span>◾<br> Jurnal</span>
+            <a href="/"><span>◾<br> Jurnal</span></a>
         </div>
         <div class="tab" id="tab-form">
-            <span>✎<br> Form</span>
+           <a href="/form"> <span>✎<br> Form</span></a>
         </div>
         <div class="tab" id="tab-list">
-            <span>☰<br> List</span>
+           <a href="/list"> <span>☰<br> List</span></a>
         </div>
     </div>
 </div>
